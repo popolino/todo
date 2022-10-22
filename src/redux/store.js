a2d56640c2781b9ebade6833b5dcb14b740e39e2
boobitch <polenafan@gmail.com>
2022-10-22 20:29:16 +0400

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import {combineReducers, createStore} from "redux";
+import toDoListReducer from "../reducers/ToDoListReducer";
+
+
+const rootReducer = combineReducers(
+  {toDoListReducer}
+)
+
+const store = createStore(rootReducer)
+
+export default store
